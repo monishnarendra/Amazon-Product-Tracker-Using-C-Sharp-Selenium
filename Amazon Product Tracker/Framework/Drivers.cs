dd9b48bdb866270164c470fb1c133ec6ba26b9cd
Monish Narendra <monish.narendra@gmail.com>
2020-07-22 20:28:25 +0530

    Created Empty Files-Drivers,DriverOptions,App_config References Added-Selenium,SeleniumSupport,GC,ConfigrationManager

---
+++ Amazon Product Tracker/Framework/Drivers.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon_Product_Tracker.Framework
+{
+    class Drivers
+    {
+        IWebDriver _driver;
+
+        public Drivers()
+        {
+            string programExecutionBrowser = ConfigurationManager.AppSettings["ProgramExecutionBrowser"];
+
+            switch (programExecutionBrowser)
+            {
+                case "IE":
+
+                    break;
+
+                case "GC":
+
+                    break;
+
+                case "FF":
+
+                    break;
+                default:
+                    // Invalid Input From App.config -- ProgramExecutionBrowser
+                    break;
+            }
+
+        }
+    }
+}
